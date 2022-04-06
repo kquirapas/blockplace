252fde8da783d1263eb0d7778ab65047744a868b
Kristian Quirapas <kmquirapas@gmail.com>
2022-04-06 12:51:45 +0800

    Initial Commit #3

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config({ path: ".env.local" });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +13,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+
+
+const	DEV_URL = process.env.DEV_ALCHEMY_URL_APIKEY;
+const	DEV_PRIVATEKEY = process.env.DEV_PRIVATEKEY;
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,4 +26,21 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+	defaultNetwork: "hardhat",
+	networks: {
+		mumbai: {
+			url: DEV_URL,
+			accounts: [`0x${DEV_PRIVATEKEY}`]
+		},
+	},
+	etherscan: {
+		apiKey: process.env.POLYGONSCAN_APIKEY
+	},
+	paths: {
+		artifacts: '../app/src/artifacts',
+		// sources: './src/contracts',
+		// cache: './src/cache',
+		// tests: './src/tests'
+	}
 };
+;
